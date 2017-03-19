35cea4edafcbf66214d78442ea6f4970462bf29f
Pottamkulam <thomas.pottamkulam@on.nokia.com>
2017-03-19 14:37:28 +0530

    first commit

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -4,9 +4,9 @@ package com.mycompany.app;
  * Hello world!
  *
  */
-public class App 
+private class App 
 {
-    private static void main( String[] args )
+    public static void main( String[] args )
     {
 		System.out.println("A NullPointerException is a drag...");
        
