18763749b2b9df51092475243fd3842c061fc341
Pottamkulam <thomas.pottamkulam@on.nokia.com>
2017-03-19 10:40:09 +0530

    second commit

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!...uuuu" );
+        System.out.println( "Hello World!...uu66666uu" );
 
         System.out.println("A NullPointerException is a drag...");
         Object myObject = null;
